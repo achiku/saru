7cedc91f0b4bd323c970bfd43efbec9eef7650b5
Akira Chiku <akira.chiku@gmail.com>
2016-12-24 16:34:48 +0900

    Add String() method to Node

--- ast.go
+++ ast.go
@@ -1,8 +1,11 @@
 package main
 
+import "bytes"
+
 // Node ast node
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement ast statement
@@ -30,6 +33,14 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 // Identifier ident
 type Identifier struct {
 	Token Token
@@ -40,6 +51,10 @@ func (i *Identifier) expressionNode() {}
 
 // TokenLiteral for ident
 func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+func (i *Identifier) String() string {
 	return i.Token.Literal
 }
 
@@ -57,6 +72,18 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
 // ReturnStatement return statement
 type ReturnStatement struct {
 	Token       Token
@@ -68,4 +95,13 @@ func (rs *ReturnStatement) statementNode() {}
 // TokenLiteral token literal
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	return out.String()
 }
